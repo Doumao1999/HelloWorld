9b1b4f929c8171133271a7ef63a99a76b3467805
Maria Ji <maria_ji_z@yahoo.com>
2017-10-09 22:43:51 -0700

    Clean up

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -17,28 +17,11 @@ namespace HelloWorld
 
         public static void SetMainPage()
         {
-            //Current.MainPage = new TabbedPage
-            //{
-            //    Children =
-            //    {
-            //        new NavigationPage(new ItemsPage())
-            //        {
-            //            Title = "Browse",
-            //            Icon = Device.OnPlatform("tab_feed.png",null,null)
-            //        },
-            //        new NavigationPage(new AboutPage())
-            //        {
-            //            Title = "About",
-            //            Icon = Device.OnPlatform("tab_about.png",null,null)
-            //        },
-            //    }
-            //};
+            
 
             Current.MainPage = new NavigationPage(new HomePage())
             {
-                Title = "Hello World!",
-                Icon = Device.OnPlatform("tab_feed.png", null, null)
-
+                Title = "Hello World!"
             };
 
         }
