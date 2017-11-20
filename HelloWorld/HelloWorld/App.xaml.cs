9acbabdc8f1cf1a7b0d91edf9c023e0de487cadd
maria ji <maria_ji_z@yahoo.com>
2017-11-20 10:03:12 -0800

    different remote branch

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -22,6 +22,7 @@ namespace HelloWorld
             Current.MainPage = new NavigationPage(new HomePage())
             {
                 Title = "Hello World! + Server-Local+ HiLocal+233+233"
+                // push to differentremote branch
                 // change in server
                 // add server
                 // add local
